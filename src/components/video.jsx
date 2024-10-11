798ee48b574c06dc53922d0f54fe7d4a1e73c49f
shaurya-tiwari <shauryatiwari120@gmail.com>
2024-10-11 21:02:21 +0530

    UI

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { FcMusic } from "react-icons/fc";
+import { BsSearch } from "react-icons/bs";
 
 function Vdo() {
 
@@ -8,12 +10,12 @@ function Vdo() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         const searchQuery = e.target.elements.search.value;
- 
+
         if (searchQuery.trim() === '') {
             return;
-        } 
-        const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${import.meta.env.VITE_APIkey}&q=${searchQuery}&part=snippet&type=video&maxResults=5`;
-  
+        }
+        const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${import.meta.env.VITE_APIkey}&q=${searchQuery}&part=snippet&type=video&maxResults=20`;
+
         try {
             const response = await fetch(apiUrl);
             if (!response.ok) {
@@ -46,12 +48,15 @@ function Vdo() {
     };
 
     return (
-        <div className=' align-center justify-center'>
+        <div className=' w-[372px] p-4'>
+            <h1 className='text-3xl '>< FcMusic /></h1>
+            <h1 className='font-[kiona] font-bold text-2xl'>APi.VDO</h1>
             <form onSubmit={handleSubmit}>
-                <input className='my-5 rounded-lg bg-slate-700' type="search" name="search" value={search} onChange={(e) => setSearch(e.target.value)} />
-                <button className='mx-3' type="submit">Q</button>
+                <input className='rounded-sm p-1 my-2 text-black border border-[#f7026d]' type="search" name="search" value={search} placeholder='Search anything' onChange={(e) => setSearch(e.target.value)} />
+                <button className='mx-3 text-2xl text-[#f7026d]' type="submit"><BsSearch />
+                </button>
             </form>
-            <div>
+            <div >
                 {videos}
             </div>
         </div>
